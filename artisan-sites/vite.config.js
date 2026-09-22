4459686ec7c7a87edbd54e7d370b07dbc9fbcb1c
luciecreations60 <luciecreations60@gmail.com>
2026-09-22 12:51:38 +0200

    Update vite.config.js

--- artisan-sites/vite.config.js
+++ artisan-sites/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/sites-deep/', // Doit correspondre au nom de ton dépôt GitHub
 });
