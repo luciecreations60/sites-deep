6335ba138226aed9f21648c569d35d4351415966
luciecreations60 <luciecreations60@gmail.com>
2026-09-22 15:24:20 +0200

    Update App.jsx

--- artisan-sites/src/App.jsx
+++ artisan-sites/src/App.jsx
@@ -1,26 +1,27 @@
 import { Routes, Route } from 'react-router-dom';
 import ScrollToTop from './components/ScrollToTop.jsx';
+import BackToTop from './components/BackToTop.jsx';
 import Home from './pages/Home.jsx';
 import Demos from './pages/Demos.jsx';
 import Contact from './pages/Contact.jsx';
 import Essentiel from './pages/demos/Essentiel.jsx';
 import Plus from './pages/demos/Plus.jsx';
 import Pro from './pages/demos/Pro.jsx';
-import BackToTop from './components/BackToTop.jsx';
 
 export default function App() {
   return (
     <>
       <ScrollToTop />
       <BackToTop />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/demos" element={<Demos />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/demo/essentiel" element={<Essentiel />} />
-      <Route path="/demo/plus" element={<Plus />} />
-      <Route path="/demo/pro" element={<Pro />} />
-      <Route path="*" element={<Home />} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/demos" element={<Demos />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/demo/essentiel" element={<Essentiel />} />
+        <Route path="/demo/plus" element={<Plus />} />
+        <Route path="/demo/pro" element={<Pro />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </>
   );
 }
